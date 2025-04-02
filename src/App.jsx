4bfa5948d0b80ee7618ebf77a37c3b47504bad00
haakoaal <haakoaal@stud.ntnu.no>
2025-04-02 12:07:39 +1000

    Logo and placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 //import React from 'react';
 import './App.css'
+import { Navbar, NavbarBrand } from 'reactstrap'
 
 function App() {
 
   return (
     <>
+      <Navbar color="dark" dark fixed='top'>
+        <NavbarBrand >
+          <img className='logo' src="/animalWell.png" />
+        </NavbarBrand>
+        <div className='whitetext'>
+          Some Placeholder
+        </div>
+        <div className='whitetext'>
+          Another Placeholder
+        </div>
+        <div className='whitetext'>
+          Placeholder
+        </div>
+      </Navbar>
     </>
   )
 }
