abf4671c12653ba4d45867407e281e95c62baafc
haakoaal <haakoaal@stud.ntnu.no>
2025-04-03 13:01:12 +1000

    Navbar updated to reflect vision

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,93 @@
 //import React from 'react';
+import { useState } from 'react'
 import './App.css'
-import { Navbar, NavbarBrand } from 'reactstrap'
+import { Button, FormGroup, Input, Form, Label, Modal, ModalBody, ModalFooter, ModalHeader, Navbar, NavbarBrand, Col, Container, Row, Collapse, NavbarToggler, NavItem, Nav, NavLink } from 'reactstrap'
 
 function App() {
 
+  const [modal, setModal] = useState(false)
+  const [collapse, setCollapse] = useState(false)
+
+  const toggle = () => setModal(!modal);
+  const toggleNav = () => setCollapse(!collapse)
+
   return (
     <>
-      <Navbar color="dark" dark fixed='top'>
+      <Navbar className='nav' color="dark" expand="md" dark>
         <NavbarBrand >
           <img className='logo' src="/animalWell.png" />
         </NavbarBrand>
-        <div className='whitetext'>
-          Some Placeholder
-        </div>
-        <div className='whitetext'>
-          Another Placeholder
-        </div>
-        <div className='whitetext'>
-          Placeholder
-        </div>
+
+        <NavbarToggler className='ms-auto me-4' onClick={toggleNav}/>
+        <Collapse className='ms-2' isOpen={collapse} navbar>
+          <Nav className='ms-auto' navbar>
+            <NavItem>
+              <NavLink>
+                Some Placeholder
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                Placeholder two
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                Final Placeholder
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+        <Button color='info' onClick={toggle}>Log in</Button>
       </Navbar>
+
+
+      <Container className='cont'>
+        <Row className='row'>
+          <Col className='column1'>
+            Explore a dense, interconnected labyrinth, and unravel its many secrets. 
+            Collect items to manipulate your environment in surprising and meaningful ways. 
+            Encounter beautiful and unsettling creatures, as you attempt to survive what lurks 
+            in the dark. There is more than what you see.
+          </Col>
+          <Col className='column2'>
+            <img className='image' src='animalWell.png'/>
+          </Col>
+        </Row>
+      </Container>
+      <Modal className='window login' isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>
+          Login
+        </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup row>
+              <Label for="username">
+                Username
+              </Label>
+              <Input 
+              id="username" 
+              name="username"
+              placeholder=""
+              type="username"/>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="password">
+                Password
+              </Label>
+              <Input 
+              id="password" 
+              name="password"
+              placeholder=""
+              type="password"/>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success">Log in</Button>
+          <Button color="danger">Cancel</Button>
+        </ModalFooter>
+      </Modal>
     </>
   )
 }
