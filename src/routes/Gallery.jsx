e839374829848ecf968949453e30018210f4ee85
haakoaal <haakoaal@stud.ntnu.no>
2025-04-12 09:44:43 +1000

    Cleaned up Gallery

--- src/routes/Gallery.jsx
+++ src/routes/Gallery.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { API_URL } from "../components/Login";
 import { Button, Container } from "reactstrap";
+import { useLogin } from "../components/LoginContext";
 const Gallery = () => {
 
-  const [person, setPerson] = useState({name:"",year:""});
-  const [name, setName] = useState("");
+  const [details, setDetails] = useState({username:"",email:"", firstname:""});
+  const [error, setError] = useState("");
+  const {toggleLogin} = useLogin();
 
-  const getPersonDetails = () => {
-    const tempPerson = "nm0002354"
-    const url = `${API_URL}/people/${tempPerson}`
+  const getDetails = () => {
+    const url = `${API_URL}/auth/me`
     const token = localStorage.getItem("token");
 
     return fetch(url, {
@@ -17,36 +18,44 @@ const Gallery = () => {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${token}`
         },
-      }).then((res) => 
+      }).then((res) => {
+        if (res.status === 401) {
+          setError(
+            "You need an account to access this content"
+          )
+        }
         res.json()
-          .then((res) => {
-            console.log(res);
-            setPerson({name: res.name, year: res.birthYear})
+        .then((res) => {
+          console.log(res);
+          setDetails({
+            username: res.username, 
+            email: res.email,
+            firstname: res.firstName,
           })
+        });
+
+      }
       )
       .catch( error => console.error(error))
   }
 
+  useEffect( () => {getDetails()}, []);
 
   return (
     <>
       <Container>
         <h1 className="text-center">Gallery</h1>
+        <div className={`d-${error ? "block" : "none"} mt-5 text-center`}>
+          <h4>{error}</h4>
+          <Button onClick={toggleLogin}>Log in</Button>
+        </div>
         
-        <div className="text-center mt-5">
-          <label htmlFor="name"> Enter name: </label>
-          <input type="text" name="name" id="name" 
-            value={name}
-            onChange={e => {
-              setName(e.target.value);
-            }}
-          />
-          <br/>
-          <Button onClick={getPersonDetails} color="primary" className="mt-3">
-            Search { name ? `for ${name} ` : ""}
-          </Button>
-          <h3>{person.name}</h3>
-          <h5>{person.year}</h5>
+        <div className={
+          `d-${details.firstname != "" ? "none" : "block"} 
+          text-center mt-5`
+        }>
+          <h3>{details.firstname}'s username:  {details.username}</h3>
+          <h5>{details.email}</h5>
         </div>
       </Container>
     </>
